779e4baec70e0fb12f21f7ce2cd791ee91eb9d05
austaras <austaras@outlook.com>
2019-04-11 11:00:06 +0800

    parse args manually

--- webpack.config.ts
+++ webpack.config.ts
@@ -6,111 +6,110 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import { LicenseWebpackPlugin } from 'license-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
-type Parameter = Record<string, any>
+const modeArg: string | undefined = process.argv
+    .filter(str => str.startsWith('--mode'))[0]
+const mode = modeArg !== undefined ?
+    modeArg.split('=')[1].trim() : 'development'
+const devMode = mode !== 'production'
 
-console.log(process.argv)
-
-const config: (env: Parameter, args: Parameter) => webpack.Configuration =
-    (_: Parameter, args: Parameter) => {
-        const devMode = args.mode !== 'production'
-        return {
-            entry: {
-                polyfills: __dirname + '/src/polyfills.ts',
-                main: [
-                    __dirname + '/src/main.ts',
-                    __dirname + '/src/styles.scss'
-                ],
-            },
-            devtool: devMode ?
-                'inline-cheap-module-source-map' : 'nosources-source-map',
-            output: {
-                path: __dirname + '/dist',
-                filename: devMode ? '[name].js' : '[name].[contenthash].js'
-            },
-            optimization: {
-                runtimeChunk: 'single',
-                splitChunks: {
-                    chunks: 'all',
-                    cacheGroups: {
-                        vendor: {
-                            test: /[\\/]node_modules[\\/]/,
-                            name: 'vendors',
-                            chunks: 'all'
-                        }
+const config: webpack.Configuration = {
+    entry: {
+        polyfills: __dirname + '/src/polyfills.ts',
+        main: [
+            __dirname + '/src/main.ts',
+            __dirname + '/src/styles.scss'
+        ],
+    },
+    devtool: devMode ?
+        'inline-cheap-module-source-map' : 'nosources-source-map',
+    output: {
+        path: __dirname + '/dist',
+        filename: devMode ? '[name].js' : '[name].[contenthash].js'
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            chunks: 'all',
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+    devServer: {
+        contentBase: __dirname + '/dist',
+        before(_, server) {
+            // watch index.html changes
+            (server as any)._watch(__dirname + '/src/index.html')
+        },
+        clientLogLevel: 'warning',
+        hot: true
+    },
+    mode: devMode ? 'development' : 'production',
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                devMode ? {
+                    loader: 'style-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                } : MiniCssExtractPlugin.loader, {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
                     }
                 }
+            ]
+        }, {
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            options: {
+                transpileOnly: devMode
             },
-            devServer: {
-                contentBase: __dirname + '/dist',
-                before(_, server) {
-                    // watch index.html changes
-                    (server as any)._watch(__dirname + '/src/index.html')
-                },
-                clientLogLevel: 'warning',
-                hot: true
-            },
-            mode: devMode ? 'development' : 'production',
-            module: {
-                rules: [{
-                    test: /\.scss$/,
-                    use: [
-                        devMode ? {
-                            loader: 'style-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        } : MiniCssExtractPlugin.loader, {
-                            loader: 'css-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        }, {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        }
-                    ]
-                }, {
-                    test: /\.tsx?$/,
-                    loader: 'ts-loader',
-                    options: {
-                        transpileOnly: devMode
-                    },
-                    include: /src/,
-                }, {
-                    test: /\.(html)$/,
-                    loader: 'html-loader'
-                }, {
-                    test: /\.(png|jpg|gif|svg|webp)$/,
-                    loader: 'url-loader',
-                    options: {
-                        name: devMode ? '[name].[ext]' : 'assets/[name].[ext]?[hash]',
-                        limit: 1024
-                    }
-                }],
-            },
-            resolve: {
-                extensions: ['.tsx', '.ts', '.js']
-            },
-            plugins: [
-                new CleanWebpackPlugin(),
-                new HtmlWebpackPlugin({
-                    template: 'src/index.html'
-                }),
-                !devMode && new MiniCssExtractPlugin({
-                    chunkFilename: 'styles.[contenthash].css'
-                }),
-                devMode && new ForkTsCheckerWebpackPlugin({
-                    tslint: true,
-                }),
-                devMode && new webpack.HotModuleReplacementPlugin(),
-                // because why not
-                !devMode && new LicenseWebpackPlugin({
-                    perChunkOutput: false
-                })
-            ].filter((i): i is webpack.Plugin => i !== false)
-        }
-    }
+            include: /src/,
+        }, {
+            test: /\.(html)$/,
+            loader: 'html-loader'
+        }, {
+            test: /\.(png|jpg|gif|svg|webp)$/,
+            loader: 'url-loader',
+            options: {
+                name: devMode ? '[name].[ext]' : 'assets/[name].[ext]?[hash]',
+                limit: 1024
+            }
+        }],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        devMode && new ForkTsCheckerWebpackPlugin({
+            tslint: true,
+        }),
+        devMode && new webpack.HotModuleReplacementPlugin(),
+        devMode || new MiniCssExtractPlugin({
+            chunkFilename: 'styles.[contenthash].css'
+        }),
+        // because why not
+        devMode || new LicenseWebpackPlugin({
+            perChunkOutput: false
+        })
+    ].filter((i): i is webpack.Plugin => typeof i !== 'boolean')
+    // see https://jsperf.com/is-boolean/3
+}
 
 export default config
