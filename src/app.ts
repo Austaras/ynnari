8de62f4ec41f066ae5b5571e035145180d5ffa21
austaras <austaras@outlook.com>
2020-02-01 20:02:58 +0800

    disable decorator

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,4 @@
-function Bar(ctor: any) {
-    return ctor
-}
-
-@Bar
-class Foo {
-    public t = 1
-}
-
 export async function App(): Promise<void> {
     await Promise.resolve()
-    console.log(new Foo())
+    console.log(123)
 }
